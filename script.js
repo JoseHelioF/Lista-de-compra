40ce2890c927b5828acca00d30a89c2468d25414
Jose Helio <joseheliofvrb@gmail.com>
2022-03-28 12:50:40 -0300

    Adição da função excluir

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const base = {};
 textoEntrada = document.getElementsByTagName("input");
 var  elemento_pai = document.getElementsByTagName("section")[0];
+let position = Object.keys(base).length;
 
 //Gerar um novo item na lista
 function gera() {
   if (textoEntrada[0].value) {
-    let position = Object.keys(base).length;
+    
     console.log(textoEntrada[0].value);
     base[position + 1] = textoEntrada[0].value;
     console.log(position);
@@ -15,6 +16,7 @@ function gera() {
   } else {
     alert("Digite um valor");
   }
+  console.log("Aqui cheguei");
   textoEntrada[0].value = "";
     
 }
@@ -32,10 +34,7 @@ function geraFilho(pai,tag){
   var tagnova = pai.appendChild(tag);
   return tagnova;
 }
-function geraIrmao(pai,tag){
-  var tagnova = pai.be(tag);
-  return tagnova;
-}
+
 
 
 //Cria um novo elemento para exibir o novo item
@@ -47,7 +46,7 @@ function criaArticle() {
   
   
   var tagP = geraElemento("p");
-  let position = Object.keys(base).length;
+  // let position = Object.keys(base).length;
   texto = base[position];
   const textNode = document.createTextNode(texto);
   var tagPF = geraFilho(tagP,textNode);
@@ -58,24 +57,22 @@ function criaArticle() {
   var tagBD = geraFilho(tagArticleF,tagD);
 
   var tagB = geraElemento("button");
-  geraAtributo(tagB,"class","btnEdit")  
+  geraAtributo(tagB,"class","btnEdit")   
   var tagBF = geraFilho(tagBD,tagB);
+  document.getElementsByClassName('btnEdit')[position+1].innerHTML = 'Editar'
 
-  var tagS = geraElemento("button");
-  geraAtributo(tagS,"stroke","currentColor")  
-  geraAtributo(tagS,"fill","currentColor")  
-  geraAtributo(tagS,"stroke-width","0")    
-  geraAtributo(tagS,"viewBox","0 0 576 512")    
-  geraAtributo(tagS,"height","1em")    
-  geraAtributo(tagS,"width","1em")     
-  geraAtributo(tagS,"xmlns","http://www.w3.org/2000/svg")  
-  var tagSF = geraFilho(tagBF,tagS);
-
-  
-  var tagP = geraElemento("path");
-  geraAtributo(tagP,"d","M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z")  
-  var tagPF = geraFilho(tagSF,tagP);
-
- 
-  console.log(tagSF);
+  var tagB = geraElemento("button");
+  geraAtributo(tagB,"class","btnDelete") 
+  geraAtributo(tagB,"onclick","excluir(this)")   
+  var tagBF = geraFilho(tagBD,tagB);
+  document.getElementsByClassName('btnDelete')[position+1].innerHTML = 'Excluir'
 }
+
+function excluir(valor){
+  let x = valor.parentNode.parentNode
+  console.log(valor , x);
+  x.parentNode.removeChild(x);
+  console.log(valor , x);
+  
+}
+
