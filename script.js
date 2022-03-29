4bfbcda5bca8c30af0ea8413bfed126b68873b96
Jose Helio <joseheliofvrb@gmail.com>
2022-03-29 12:51:35 -0300

    Teste com array

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const base = {};
+const base = [];
+
+console.log(typeof(base));
 textoEntrada = document.getElementsByTagName("input");
 var  elemento_pai = document.getElementsByTagName("section")[0];
 let position = Object.keys(base).length;
@@ -7,16 +9,16 @@ let position = Object.keys(base).length;
 function gera() {
   if (textoEntrada[0].value) {
     
-    console.log(textoEntrada[0].value);
-    base[position + 1] = textoEntrada[0].value;
-    console.log(position);
+    // console.log(textoEntrada[0].value);
+    base[position + 1] = [position + 1,textoEntrada[0].value];
+    // console.log(position);
     position++;
-    console.log(base);
+    // console.log(base);
     criaArticle();
   } else {
     alert("Digite um valor");
   }
-  console.log("Aqui cheguei");
+  // console.log("Aqui cheguei");
   textoEntrada[0].value = "";
     
 }
@@ -47,7 +49,7 @@ function criaArticle() {
   
   var tagP = geraElemento("p");
   // let position = Object.keys(base).length;
-  texto = base[position];
+  texto = base[position][1];
   const textNode = document.createTextNode(texto);
   var tagPF = geraFilho(tagP,textNode);
   geraFilho(tagArticleF,tagP);
@@ -71,8 +73,31 @@ function criaArticle() {
 function excluir(valor){
   let x = valor.parentNode.parentNode
   console.log(valor , x);
+  console.log("xxx",valor , x.innerHTML);
   x.parentNode.removeChild(x);
-  console.log(valor , x);
   
+
+  position--;
+  console.log(base);
 }
 
+
+
+let lista = [];
+
+lista = [[1,2],[3,4],[5,6],[7,8],[9,10]]
+console.log(lista);
+let item = 6;
+var newt = [];
+for (let  i of lista){
+  if(i[1]!=6){
+    newt.push(i);
+   }
+ 
+  console.log("lista",i[1]);
+}
+newt[5] = [11,12];
+console.log(newt);
+// for (let i = 0; i< lista.length;i++){
+//   console.log(lista[i][1]);
+// }
